a6b13231b12345511838770813cd8c492c9fb764
benjaminsunderland <ben@sunderland.eu.com>
2017-09-26 15:36:42 +0100

    Added tests for number 5, number 3, and a number which is divisble by both

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,10 +1,42 @@
 function Javabuzz() {
 
+
 };
 
 Javabuzz.prototype.isDivisbleByThree = function(number) {
+  if (number == 3) {
    return true
+ } else {
+   return false
+ }
+};
+
+Javabuzz.prototype.isDivisbleByFive = function(number) {
+  if (number == 5) {
+   return true
+ } else {
+   return false
+ }
+};
+
+Javabuzz.prototype.isDivisbleByThreeAndFive = function(number) {
+
+  if (number % 3 == 0 && number % 5 == 0) {
+    return true
+  } else {
+    return false
+  }
+};
+
+
+
+Javabuzz.prototype.isDivisbleBy = function(number, divisor) {
+
+  if (number % divisor == 0) {
+    return true
+  } else {
+    return false
+  }
 };
 
 var javabuzz = new Javabuzz();
-console.log(javabuzz.isDivisbleByThree(3));
